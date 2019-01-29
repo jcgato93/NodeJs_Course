812c384ac6ce6a51cbaf58c088496709353349f4
Camilo.IT <npgcamilo@gmail.com>
2019-01-29 18:50:17 -0500

    Modulos en Node.js: require() y module.exports

---
+++ Examples/index.js
@@ -0,0 +1,17 @@
+/**
+ *  **********
+ *  Comience aqui
+ *  **********
+ */
+
+ const math = require("./math");
+ const greet = require("./greetings");
+ const hello = require("./greetings/hello");
+
+ console.log(math.add(4,10));
+ console.log(math.substract(4,8));
+ console.log(math.multiply(4,8));
+ console.log(math.divide(8,4));
+
+
+ console.log(greet.greet('Juan'));
