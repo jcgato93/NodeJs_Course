a6e729eb7ccd72b16d097b4c3bf5893bab783341
jcgato93 <npgcamilo@gmail.com>
2019-02-09 16:22:00 -0500

    FileSystem Read and Write files

---
+++ Examples/4- fileSystem read and write files.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const fs = require("fs");
+
+function incrementValues(list){
+    const mapIncrement = val => Number(val) +1;
+    return list.map(mapIncrement)
+}
+
+
+
+/**
+* contenido del archivo que se lee
+1
+2
+3....
+
+
+Resultado esperado 
+[2,3,4,5....]
+*/
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf8', (err, list) => {
+        if (err) reject(err);
+  
+        resolve(list.split('\n'));
+      });
+    });
+  }
+  
+  const writeFile = (path, value) => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(path, value, (err, list) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  }
+  
+  const main = async function() {
+      try {
+        const fileReaded= await readFile('Resources/numbers.txt')
+        const newNumbers = incrementValues(fileReaded.map(n => Number(n)));
+        const save = await writeFile('Resources/numbernew.txt', newNumbers)
+        
+        console.log(save)
+      } catch (error) {
+        console.log(error)
+      }   
+  }
+  
+  main();
